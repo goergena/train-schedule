43892637d86693be03a39f78726cedcc151db03b
Ali <ali@Alis-MBP.tremontplacelofts.local>
2018-03-13 13:36:20 -0400

    app works without database. next fix: showing time with AM PM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,121 +1,179 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAeSEQnspIfuu39mrcXEmVWVUeKe2GKcn8",
-    authDomain: "train-schedule-1ec1e.firebaseapp.com",
-    databaseURL: "https://train-schedule-1ec1e.firebaseio.com",
-    projectId: "train-schedule-1ec1e",
-    storageBucket: "",
-    messagingSenderId: "708784360533"
-};
-firebase.initializeApp(config);
+$(document).ready(function () {
 
-var database = firebase.database();
 
-//okay i thik im going to have an array of objects and then
-//do a for loop to add each one to the database
-//and a for loop to display each one
-
-//1. when you add a train, it adds it to the array.
-//no, it's supposd to be stored as data. well it can be a local variable AND stored as data.
-//okay when you add a train it adds it to the array. 
-//the data object grabs from the array to store data.
-//the display grabs from the data to display.
-//this happens on submit and on page refresh. 
-var trains = [{
-        name: "Thomas the Tank Engine",
-        destination: "Lazy Town",
-        firstTime: "8:00",
-        frequency: 120,
-    },
-    {
-        name: "Cali Express",
-        destination: "San Francisco",
-        firstTime: "6:00",
-        frequency: 30,
-    }
-];
-
-$("#submit-Btn").on("click", function () {
-    var newTrain = {
-        name: $("#name-input").val().trim(),
-        destination: $("#destination-input").val().trim(),
-        firstTime: $("#first-input").val().trim(),
-        frequency: $("#freq-input").val().trim(),
+    // Initialize Firebase
+    /* var config = {
+        apiKey: "AIzaSyAeSEQnspIfuu39mrcXEmVWVUeKe2GKcn8",
+        authDomain: "train-schedule-1ec1e.firebaseapp.com",
+        databaseURL: "https://train-schedule-1ec1e.firebaseio.com",
+        projectId: "train-schedule-1ec1e",
+        storageBucket: "",
+        messagingSenderId: "708784360533"
     };
-    trains.push(newTrain);
-});
+    firebase.initializeApp(config);
 
-$("#time").append(Date());
+    var database = firebase.database();
+    */
 
-function currentTime() {
-    var d = new Date();
-    var h = d.getHours();
-    var m = d.getMinutes();
-    var timeDisplay = h + ":" + m + " AM";
-    if (m < 10) {
-        var newM = "0" + m;
-        console.log(newM);
-    }
+    //okay i thik im going to have an array of objects and then
+    //do a for loop to add each one to the database
+    //and a for loop to display each one
 
-    if (h > 12) {
-        var newH = h - 12;
-        timeDisplay = newH + ":" + m + " PM";
-    }
-    console.log(timeDisplay);
-    return timeDisplay;
-};
+    //1. when you add a train, it adds it to the array.
+    //no, it's supposd to be stored as data. well it can be a local variable AND stored as data.
+    //okay when you add a train it adds it to the array. 
+    //the data object grabs from the array to store data.
+    //the display grabs from the data to display.
+    //this happens on submit and on page refresh. 
 
-currentTime();
+    //currentTime();
+    var trains = [{
+            name: "Thomas the Tank Engine",
+            destination: "Lazy Town",
+            firstTime: "8:00",
+            frequency: 120,
+        },
+        {
+            name: "Cali Express",
+            destination: "San Francisco",
+            firstTime: "6:00",
+            frequency: 30,
+        },
+        {
+            name: "a",
+            destination: "b",
+            firstTime: "4:00",
+            frequency: 20,
+        },
+    ];
+/*
+    $("#new-btn").on("click", function(){
+        var secondDiv = $("<div>");
+        secondDiv.text("hey there");
+        $("#new-div").append(secondDiv);
+        var name = $("#name-input").val().trim();
+        var destination = $("#destination-input").val().trim();
+        var firstTime =  $("#first-input").val().trim();
+        var frequency = $("#freq-input").val().trim();
 
-var nextArrival = 0;
-var minAway = 0;
+        var newTrain = {
+            name: name,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency,
+        }; 
+        trains.push(newTrain);
+        console.log(newTrain); 
+        displayTrains();
+
+    }) */
+
+    $("#submit-btn").on("click", function () {
+        var name = $("#name-input").val().trim();
+        var destination = $("#destination-input").val().trim();
+        var firstTime =  $("#first-input").val().trim();
+      //  console.log("hey first time " + firstTime);
+        var frequency = $("#freq-input").val().trim();
+
+        var newTrain = {
+            name: name,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency,
+        }; 
+        trains.push(newTrain);
+       // console.log(newTrain); 
+        displayTrains();
+    }); 
+  
 
 
-function calculateNextArrival(x, y) {
-    //x will be first train time
-    //y will be freq;
-    var arr = x.split(":");
-    var milHours = parseInt(arr[0]);
-    var milMin = parseInt(arr[1]);
-    var firstInMin = milHours * 60 + milMin;
-    console.log("first arrival:" + firstInMin);
-    //console.log(typeof firstInMin);
+    function displayTrains() {
+         $("tbody").empty();
+        trains.forEach(function (train) {
+            var row = $("<tr>");
+            var head = $("<th scope='row'</th>");
+            head.text(train.name);
+            var dest = $("<td>");
+            dest.text(train.destination);
+            var freq = $("<td>");
+            freq.text(train.frequency);
+            var next = $("<td>");
+            //calculateNextArrival(train.firstTime, train.frequency);
+            var currentTime = calcTime();
+            console.log(typeof currentTime);
+            var nextArrivalInMin = calculateNextArrival(train.firstTime, train.frequency, currentTime);
+           // console.log(nextArrivalInMin);
+            var nextArrivalConverted = timeConverter(nextArrivalInMin);
+            console.log(nextArrivalConverted);
+            next.text(nextArrivalConverted); 
+            var wait = $("<td>");
+           wait.text(calcWait(nextArrivalInMin, currentTime)); 
+            row.append(head, dest, freq, next, wait);
 
-    var date = new Date();
-    var curTimeInMin = date.getHours() * 60 + date.getMinutes();
-    console.log("current time in min" + curTimeInMin);
-
-
-    while (firstInMin < curTimeInMin) {
-        firstInMin += y;
+            $("tbody").append(row);
+        })
     };
-    if (firstInMin >= curTimeInMin) {
-        nextArrival = firstInMin;
-        minAway = nextArrival - curTimeInMin;
+
+    displayTrains();
+
+
+    function calcTime() {
+
+        var d = new Date();
+        console.log(d.getHours() * 60 + d.getMinutes());
+        return d.getHours() * 60 + d.getMinutes();
+
     }
 
-    console.log("next arrival in min" + nextArrival);
+    function calculateNextArrival(x, y, z) {
+        //x will be first train time
+        //y will be freq;
+        //z will be current Time
+        var arr = x.split(":");
+        var milHours = parseInt(arr[0]);
+        console.log(milHours + " this is hours");
+        var milMin = parseInt(arr[1]);
+        var firstInMin = milHours * 60 + milMin;
+        //console.log("first arrival:" + firstInMin);
+        console.log(typeof firstInMin);
+       // var currentFrequency = parseInt(y);
 
-};
+        while (firstInMin < z) {
+            firstInMin += parseInt(y);
+        };
+        if (firstInMin >= z) {
+            var nextArrival = firstInMin;
+            console.log(nextArrival);
+            console.log(typeof firstInMin);
+            
+            return nextArrival;
+        }
 
-function timeConverter(z) {
-    //z will be nextArrival
-    if(z >= 60) {
+    };
+
+    function calcWait(a, b) {
+        return a - b;
+    }
+
+    function timeConverter(z) {
+        //z will be nextArrival
+
         var convertedHours = Math.floor(z / 60);
-        var convertedMin = z % 60;
-        console.log(convertedHours + ":" + convertedMin);
-    }
-}
-timeConverter(912);
+        while (convertedHours>=24) {
+            convertedHours-=24;
+        }
 
-var testArrival = "5:00";
-var testFreq = 60;
-var testArrival2 = "15:00";
-var testArrival3 = "14:52";
-calculateNextArrival(testArrival, testFreq);
-calculateNextArrival(testArrival2, testFreq);
-calculateNextArrival(testArrival3, 7);
+        if (z < 60) {
+            convertedHours = "00";
+        }
+        var convertedMinNum = z % 60;
+        if (z % 60 < 10) {
+            convertedMin = "0" + convertedMinNum;
+            return convertedHours + ":" + convertedMin;
+        }
+        return convertedHours + ":" + convertedMinNum;
+    };
+    
 
-
-
-
+});
